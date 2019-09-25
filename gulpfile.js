4a36683d6c47546c22dcbb43a446f983e329d15f
alice <itany@163.com>
2019-09-25 10:56:31 +0800

    init project

--- gulpfile.js
+++ gulpfile.js
@@ -1,138 +1,63 @@
 let gulp = require("gulp");
 
 let app = {
-    src: "./src",
-    dist: "./dist"
+    src: "./node_modules/bootstrap",
+    dist: "./node_modules/bootstrap/dist",
+    js: [
+         './node_modules/bootstrap/js/transition.js',
+          './node_modules/bootstrap/js/alert.js',
+          './node_modules/bootstrap/js/button.js',
+          './node_modules/bootstrap/js/carousel.js',
+          './node_modules/bootstrap/js/collapse.js',
+          './node_modules/bootstrap/js/dropdown.js',
+          './node_modules/bootstrap/js/modal.js',
+          './node_modules/bootstrap/js/tooltip.js',
+          './node_modules/bootstrap/js/popover.js',
+          './node_modules/bootstrap/js/scrollspy.js',
+          './node_modules/bootstrap/js/tab.js',
+          './node_modules/bootstrap/js/affix.js'
+    ]
 }
 
-// html压缩
-// npm i gulp-htmlmin --save-dev
-let htmlmin = require("gulp-htmlmin");
+gulp.task("copy", function(done) {
+    // gulp.src(app.src)               // 只会拷贝src目录
+    // gulp.src(app.src + "/*")            // 只会拷贝src目录下的一级内容（子文件夹内容没有，src目录没有创建）
+    // gulp.src(app.src + "/**")            // 会拷贝src目录下的所有内容（src目录没有创建）
 
-gulp.task("htmlmin",function(done){
-    gulp.src(`${app.src}/**/*.{htm,html}`)
-        .pipe(htmlmin({
-            removeComments:true,
-            collapseWhitespace:true,
-            collapseBooleanAttributes:true,
-        }))
+    gulp.src(app.src+"/fonts")
         .pipe(gulp.dest(app.dist));
-    done();
-})
-
-// css压缩
-// npm i gulp-cssmin --save-dev
-
-let cssmin = require("gulp-cssmin");
-
-gulp.task("cssmin",function(done){
-
-    gulp.src(`${app.src}/**/*.css`)
-        .pipe(cssmin())
-        .pipe(gulp.dest(app.dist));
+    gulp.src(app.src + "/fonts/**")
+        .pipe(gulp.dest(app.dist + "/fonts"));
 
     done();
-})
-
+});
 // js压缩混淆
 // npm i gulp-uglify-es --save-dev
 
-
-//方式一
-// let jsmin = require("gulp-uglify-es");
-
-// gulp.task("jsmin",function(done){
-
-//     gulp.src(`${app.src}/**/*.js`)
-//         .pipe(jsmin.default())
-//         .pipe(gulp.dest(app.dist));
-
-//     done();
-// })
-
-//方式二
-let { default: jsmin } = require("gulp-uglify-es");
-
-
-gulp.task("jsmin",function(done){
-
-    gulp.src(`${app.src}/**/*.js`)
-        .pipe(jsmin())
-        .pipe(gulp.dest(app.dist));
-
-    done();
-})
-
-
-// 文件重命名（实现src拷贝到bak目录下，所有文件后缀名都附加一个.bak）
-// npm i gulp-rename -D
-
-let rename = require("gulp-rename");
-gulp.task("rename",function(done){
-
-    gulp.src(`${app.src}/**`)
-        .pipe(rename(function(target){
-            if(target.extname)
-                target.extname +=".bak";
-        }))
-        .pipe(gulp.dest(app.dist));
-
-    done();
-})
-
-// 文件合并
-// npm i gulp-concat -D
-
+let jsmin = require("gulp-uglify-es");
 let concat = require("gulp-concat");
+let rename = require("gulp-rename");
+let { default: uglify } = require("gulp-uglify-es");
 
-// 将src根目录下的所有css文件合并为all.css
-
-
-gulp.task("concat",function(done){
-
-    gulp.src(`${app.src}/**/*.css`)
-        .pipe(concat("all.css"))
-        .pipe(cssmin())
-        .pipe(gulp.dest(app.dist));
+gulp.task("js", function(done) {
+    gulp.src(app.js)
+        .pipe(concat("bootstrap.js"))
+        .pipe(uglify())
+        .pipe(rename("bootstrap.min.js"))
+        .pipe(gulp.dest(`${app.dist}/js`));
 
     done();
-})
+});
 
-
-// less编译
-// npm i gulp-less -D
 
 let less = require("gulp-less");
 
 // 将src/less/*.less编译到dist/css/*.css
 gulp.task("less",function(done){
-
-    gulp.src(`${app.src}/less/*.less`)
+   
+    gulp.src(`${app.src}/less/bootstrap.less`)
         .pipe(less())
        
-        .pipe(gulp.dest(app.dist + "/css"));
-
-    done();
-})
-
-// 将src/less/*.less编译到dist/css/all.css
-gulp.task("less2",function(done){
-
-    gulp.src(`${app.src}/less/*.less`)
-        .pipe(less())
-        .pipe(concat("all.css"))
-        .pipe(gulp.dest(app.dist + "/css"));
-
-    done();
-})
-// 将src/less/*.less编译并压缩到dist/css/all.min.css
-gulp.task("less3",function(done){
-
-    gulp.src(`${app.src}/less/*.less`)
-        .pipe(less())
-        .pipe(concat("all.css"))
-        .pipe(cssmin())
-        .pipe(rename("all.min.css"))
         .pipe(gulp.dest(app.dist + "/css"));
 
     done();
